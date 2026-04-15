44ccfb5f77f0ea83186b79078c0788f38a86eaba
Preet-kasana205git config --global user.email kasanap553@gmail.com <kasanap553@gmail.com>
2026-04-15 10:53:11 +0530

    update code to user input based

--- scanner.py
+++ scanner.py
@@ -2,16 +2,33 @@ import socket
 
 def scan_port(target, port):
     scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    scanner.settimeout(1)
+    scanner.settimeout(1) # we can set a timeout for the connection attempt for faster scanning 0.5 seconds or 1 second is usually good for most cases
     result = scanner.connect_ex((target, port))
     scanner.close()
-
     if result == 0:
-        print(f"Port {port} is OPEN ✅")
+        return True
     else:
-        print(f"Port {port} is CLOSED ❌")
+        return False
 
-target = socket.gethostbyname("google.com")
-scan_port(target, 80)
-scan_port(target, 443)
-scan_port(target, 9999)
+def main():
+    target_name = input("Enter target : ")
+    target = socket.gethostbyname(target_name)
+
+    start_port = int(input("Enter start port: "))
+    end_port = int(input("Enter end port: "))
+
+    print(f"\nScanning {target_name} ({target})...")
+    print(f"Scanning ports {start_port} to {end_port}")
+    print("-" * 40)
+
+    open_ports = []
+
+    for port in range(start_port, end_port + 1):
+        if scan_port(target, port):
+            print(f"Port {port} is OPEN ✅")
+            open_ports.append(port)
+
+    print("-" * 40)
+    print(f"Scan complete! Found {len(open_ports)} open ports.")
+
+main()
